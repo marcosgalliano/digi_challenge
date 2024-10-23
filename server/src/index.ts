5526ab65106d34d7e5c331ca71e6bda72deb0eb5
marcos galliano <marquitosgalliano@hotmail.com>
2024-10-23 18:58:02 -0300

    cors access

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "./routes/index";
-import morgan from 'morgan';
+import morgan from "morgan";
 import connectDB from "./db";
 import { saveDataCronJob } from "./cronJobs";
 import cors from "cors";
@@ -9,9 +9,9 @@ const app = express();
 connectDB();
 saveDataCronJob();
 
-app.use(cors({ origin: "http://localhost:3000" })); 
+app.use(cors({ origin: "https://digi-challenge.vercel.app/" })); //http://localhost:3000
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api", routes);
 
